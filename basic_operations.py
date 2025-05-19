41b47367f6e56bf9316e3829bc8850457e5c5b25
medatik <mohamed.atikeddine@gmail.com>
2025-05-19 22:21:57 +0100

    created basic_operations.py and wrote the script

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number1 - number2
+product = number1 * number2
+
+print(f"the sum of {number1} and {number2} is : {sum}")
+print(f"the subttracting of {number2} from {number1} is : {sub}")
+print(f"the product of {number1} and {number2} is : {product}")
