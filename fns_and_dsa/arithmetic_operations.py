78194fd9e06c9398f19a74de927baeb6628127dc
medatik <mohamed.atikeddine@gmail.com>
2025-06-08 22:01:32 +0100

    hello

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,16 @@
-def perform_operation (num1, num2, operation) :
+def perform_operation (num1: float, num2: float, operation: str) :
     match operation :
         case 'add':
-            print(f"the addition of the two numbers is {num1 + num2}")
+            return num1 + num2
         case 'subtract':
-            print(f"the substruction is : {num1 - num2}")
+            return num1 - num2
         case 'multiply':
-            print(f"the multiplication is : {num1 * num2}")
+            return num1 * num2
         case 'divide' :
             if num2 != 0 : 
-                print(f" the division is : {num1 / num2}")
+                return num1 / num2
             else :
-                print("can't divide by 0")
+                return "can't divide by 0"
         case _ :
             print("operation must be add, substract, multiply or divide")
             
