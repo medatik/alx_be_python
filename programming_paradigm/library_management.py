4d079cc31308823070b6f7191907bb1908514f4d
= <=>
2025-06-15 22:46:51 +0100

    library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,50 @@
+class Book:
+    """A class to represent a book in the library."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    """A class to represent a library and manage its collection of books."""
+
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title if available."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return
+
+    def list_available_books(self):
+        """List all books that are currently available."""
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
