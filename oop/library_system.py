f63170cf1a662d4e1b924aa021224502e85f59f6
medatik <mohamed.atikeddine@gmail.com>
2025-06-20 11:40:04 +0100

    edit lib

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,24 +1,23 @@
-class Book :
-    def __init__(self, title, author) :
+class Book:
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
-class EBook(Book) :
+class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
-
-class PrintBook(Book) :
-    def __init__(self, title, author, page_count ):
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
-        self.page_count  = page_count 
+        self.page_count = page_count
 
-class Library :
+class Library:
     def __init__(self):
         self.books = []
 
-    def add_book(self, book) :
+    def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
